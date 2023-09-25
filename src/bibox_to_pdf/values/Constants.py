cf057aa52b76049c04e2c9a1e62aef0a9125267f
Lasse15 <lasse.roth@lasse-it.de>
2023-09-25 13:40:36 +0200

    Add configurable base output path as env variable

--- src/bibox_to_pdf/values/Constants.py
+++ src/bibox_to_pdf/values/Constants.py
@@ -1,9 +1,12 @@
+import os
+
+
 class Constants:
     biboxLoginUrl = 'https://bibox2.westermann.de'
     biboxBookInfoUrl = 'https://backend.bibox2.westermann.de/v1/api/sync/{}?materialtypes[]=default&materialtypes[]=addon'
-    # biboxBookPageUrl = 'https://bibox2.westermann.de/book/{{bookId}}/page/{pageNumber}'
 
-    bookBaseOutputDir = '../books/{}'
+    baseOutputPath = os.getenv('BASE_OUTPUT_PATH', default='..')
+    bookBaseOutputDir = baseOutputPath + '/books/{}'
     imageOutputDir = bookBaseOutputDir + '/images/'
     imageOutputFile = imageOutputDir + '{}.png'
     pdfOutputDir = bookBaseOutputDir + '/pdfs/'
