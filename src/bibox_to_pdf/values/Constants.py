812c08102208d8945850b447033fddb6525e4487
Lasse15 <lasse.roth@lasse-it.de>
2023-09-27 11:05:39 +0200

    Create books directory at current directory instead of upper

--- src/bibox_to_pdf/values/Constants.py
+++ src/bibox_to_pdf/values/Constants.py
@@ -5,7 +5,7 @@ class Constants:
     biboxLoginUrl = 'https://bibox2.westermann.de'
     biboxBookInfoUrl = 'https://backend.bibox2.westermann.de/v1/api/sync/{}?materialtypes[]=default&materialtypes[]=addon'
 
-    baseOutputPath = os.getenv('BASE_OUTPUT_PATH', default='..')
+    baseOutputPath = os.getenv('BASE_OUTPUT_PATH', default='.')
     bookBaseOutputDir = baseOutputPath + '/books/{}'
     imageOutputDir = bookBaseOutputDir + '/images/'
     imageOutputFile = imageOutputDir + '{}.png'
